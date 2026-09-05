42d809c739332c05c3d540e95f99bb2786b13908
anonXsmokey <153396480+anonXsmokey@users.noreply.github.com>
2026-09-05 10:06:04 +0530

    Defer ANON shell activation until first user logon

--- build/assembly/SetupComplete.cmd
+++ build/assembly/SetupComplete.cmd
@@ -4,8 +4,10 @@ set "BASE=C:\ProgramData\ANON"
 set "LOG=%BASE%\Logs\setup-complete.log"
 if not exist "%BASE%\Logs" mkdir "%BASE%\Logs" >nul 2>&1
 >>"%LOG%" echo [%date% %time%] ANON SetupComplete started
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "C:\ProgramData\ANON\Shell\Bootstrap\ANON.Shell.Bootstrap.exe" /f >>"%LOG%" 2>&1
+rem Do not replace Winlogon Shell during SetupComplete. Windows 11 OOBE still
+rem needs the stock Explorer shell/session plumbing to finish reliably.
 reg add "HKLM\SOFTWARE\ANON\OS" /v Installed /t REG_DWORD /d 1 /f >>"%LOG%" 2>&1
 reg add "HKLM\SOFTWARE\ANON\OS" /v Version /t REG_SZ /d "Windows-native V1" /f >>"%LOG%" 2>&1
->>"%LOG%" echo [%date% %time%] ANON Winlogon shell configured
+reg add "HKLM\SOFTWARE\ANON\OS" /v ShellActivationPending /t REG_DWORD /d 1 /f >>"%LOG%" 2>&1
+>>"%LOG%" echo [%date% %time%] ANON installation marked; Winlogon shell deferred until first user logon
 exit /b 0
